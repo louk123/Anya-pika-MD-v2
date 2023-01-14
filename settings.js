98b3063945d07434b4ef208cbe09744c5c47d78d
louk123 <106319242+louk123@users.noreply.github.com>
2023-01-14 22:00:32 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,25 +7,25 @@ global.simpleBotname = "Anya" //⚠️ Only use normal and one work name here or
 
 
 //------------❤️ 𝗕𝗼𝘁 𝗱𝗲𝘁𝗮𝗶𝗹𝘀 ❤️-----------\\
-global.botname = "Queen Anya" 
+global.botname = "lbasel lmalaki" 
 global.websitex = "https://github.com/Pika4O4" // Website to be displayed //
 global.botscript = 'https://github.com/PikaBotz/Anya-pika-MD-v2' // Script link //
-global.packname = "𝐴𝑛𝑦𝑎💫 вσᡶ 𝖻ꪗ ρỉƙꪖ" // sticker watermark packname //
-global.author = "🥵3.69_ρỉƙꪖ" // sticker watermark author //
+global.packname = "الـبـاسـل الـمـلـڪـي🦇⃢⃝⃟😻" // sticker watermark packname //
+global.author = "@lo9mane lmalaki" // sticker watermark author //
 actiontitle = '࿙⚜𝑨𝒏𝒚𝒂⚜࿚' // text who displays on the top of welcome and left message //
 global.headerText = "Anya by Pika" // text for header of menu //        
      
              //-------------👑Owner & vcard 👑----------\\             
-             global.ownernumber = ['918811074852']   // Owner number //
-             global.vcardowner = ['918811074852']   // vcard owner number //
+             global.ownernumber = ['212661809542']   // Owner number //
+             global.vcardowner = ['212661809542']   // vcard owner number //
              global.ownername = "3.69_Pika"   // Owner name //
              global.ytname = "YT: ONYXPIKA"   // Yt chanel name //
              global.socialm = "𝗚𝗶𝘁𝗵𝘂𝗯 : Pika4o4"   // Github or insta name //
              global.location = "India, Assam, Dibrugarh"   // Location //
-             global.owner = ['918811074852','917355622763','919673141317']
-             global.ownertag = '918811074852'  // Tag number //
-             global.linkz = "https://wa.me/+918811074852"  // Theme url which will be displayed on whatsapp //
-             global.premium = ['918811074852']  // premium numbers //             
+             global.owner = ['212661809542','212609493719','212644425361']
+             global.ownertag = '212661809542'  // Tag number //
+             global.linkz = "https://wa.me/+212661809542"  // Theme url which will be displayed on whatsapp //
+             global.premium = ['212661809542']  // premium numbers //             
              global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark //
 
 global.prefa = ['','-']
